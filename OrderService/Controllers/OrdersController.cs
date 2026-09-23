36c6beb1365a8d19730a68cdb26fc363502b8b8f
irazirfan <irazirfan@gmail.com>
2026-09-24 01:59:28 +0600

    updated to achieve microservice to microservice call, added productservice from config instead of hardcoded in controller

--- OrderService/Controllers/OrdersController.cs
+++ OrderService/Controllers/OrdersController.cs
@@ -8,43 +8,38 @@ namespace OrderService.Controllers
     [Route("api/[controller]")]
     public class OrdersController: ControllerBase
     {
-        [HttpGet]
-        public IActionResult GetOrders()
+        private IHttpClientFactory _httpClientFactory;
+        private IConfiguration _configuration;
+
+        public OrdersController(IHttpClientFactory httpClientFactory, IConfiguration configuration) 
         {
-            return Ok(new[]
-            {
-                new Order
-                {
-                    Id = 1,
-                    ProductId = 1,
-                    Quantity = 2
-                },
-                new Order
-                {
-                    Id = 2,
-                    ProductId = 2,
-                    Quantity = 1
-                }
-            });
-        }
-
-        [HttpGet("{id}")]
-        public IActionResult GetOrder(int id)
-        {
-            var order = new Order {
-
-                Id = id,
-                ProductId =  1,
-                Quantity = 2
-            };
-
-            return Ok(order);
+            _httpClientFactory = httpClientFactory;
+            _configuration = configuration;
         }
 
         [HttpPost]
-        public IActionResult CreateOrder(Order order)
+        public async Task<IActionResult> CreateOrder(Order order)
         {
-            return Ok(order);
+            var client = _httpClientFactory.CreateClient();
+
+            var productServiceUrl = _configuration["Services:ProductService"];
+
+            var productResponse = await client.GetAsync($"{productServiceUrl}/api/products/{order.ProductId}");
+
+            if (!productResponse.IsSuccessStatusCode)
+            {
+                return BadRequest("Product not found");
+            }
+
+            var product = await productResponse.Content.ReadFromJsonAsync<Product>();
+
+            return Ok(new
+            {
+                Message = "Order created",
+                Product = product,
+                Quantity = order.Quantity
+            });
         }
+
     }
 }
--- OrderService/Program.cs
+++ OrderService/Program.cs
@@ -2,6 +2,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
+builder.Services.AddHttpClient();
+
 var app = builder.Build();
 
 app.UseHttpsRedirection();
