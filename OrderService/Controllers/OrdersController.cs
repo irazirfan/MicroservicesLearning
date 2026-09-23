729cb7d06fbf9fdc7ba475969714fe8e0397520a
irazirfan <irazirfan@gmail.com>
2026-09-24 01:07:41 +0600

    Add project files.

---
+++ OrderService/Controllers/OrdersController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OrderService.Models;
+
+
+namespace OrderService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class OrdersController: ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetOrders()
+        {
+            return Ok(new[]
+            {
+                new Order
+                {
+                    Id = 1,
+                    ProductId = 1,
+                    Quantity = 2
+                },
+                new Order
+                {
+                    Id = 2,
+                    ProductId = 2,
+                    Quantity = 1
+                }
+            });
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetOrder(int id)
+        {
+            var order = new Order {
+
+                Id = id,
+                ProductId =  1,
+                Quantity = 2
+            };
+
+            return Ok(order);
+        }
+
+        [HttpPost]
+        public IActionResult CreateOrder(Order order)
+        {
+            return Ok(order);
+        }
+    }
+}
---
+++ ProductService/Controllers/ProductsController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductService.Models;
+
+namespace ProductService.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProductsController : ControllerBase
+{
+    private static readonly List<Product> Products =
+    [
+        new Product
+        {
+            Id = 1,
+            Name = "Laptop",
+            Price = 1000
+        },
+        new Product
+        {
+            Id = 2,
+            Name = "Keyboard",
+            Price = 50
+        },
+        new Product
+        {
+            Id = 3,
+            Name = "Mouse",
+            Price = 25
+        }
+    ];
+
+    [HttpGet]
+    public ActionResult<IEnumerable<Product>> GetProducts()
+    {
+        return Ok(Products);
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult<Product> GetProduct(int id)
+    {
+        var product = Products.FirstOrDefault(p => p.Id == id);
+
+        if (product == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(product);
+    }
+}
