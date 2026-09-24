376c7e50f440a467ceda1b27360ef33702c7a6df
irazirfan <irazirfan@gmail.com>
2026-09-25 03:31:02 +0600

    add Dockerized microservices with persistent database storage

--- OrderService/Program.cs
+++ OrderService/Program.cs
@@ -4,13 +4,19 @@ using OrderService.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<OrderDbContext>(options =>
-    options.UseSqlite("Data Source=orders.db"));
+    options.UseSqlite("Data Source=/data/orders.db"));
 
 builder.Services.AddControllers();
 
 builder.Services.AddHttpClient();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<OrderDbContext>();
+    db.Database.Migrate();
+}
 
 app.UseHttpsRedirection();
 
--- ProductService/Program.cs
+++ ProductService/Program.cs
@@ -4,11 +4,17 @@ using ProductService.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<ProductDbContext>(options =>
-    options.UseSqlite("Data Source=products.db"));
+    options.UseSqlite("Data Source=/data/products.db"));
 
 builder.Services.AddControllers();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<ProductDbContext>();
+    db.Database.Migrate();
+}
 
 app.UseHttpsRedirection();
 
